51c77d510c70eb9c0d43611a8700dbc2afef9db8
Aykut Vercan <aykutvercan@hotmail.com>
2023-07-20 14:34:07 +0300

    redux

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -943,7 +943,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58161" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61548" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
